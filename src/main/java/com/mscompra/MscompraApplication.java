5f23c5b970d14177999f480e17f3965214002656
Natan <natan.nbg@gmail.com>
2024-02-22 16:42:13 -0300

    creation of the project

---
+++ src/main/java/com/mscompra/MscompraApplication.java
@@ -0,0 +1,13 @@
+package com.mscompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MscompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MscompraApplication.class, args);
+	}
+
+}
